0e5f38f2d9e5786c3e37c1e28b1f63ea17cc2d6b
Hamza <hamzashoukat8080@gmail.com>
2024-10-28 00:20:15 +0500

    npx react & vite react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from "./Chai";
+
+
+function App() {
+  return (
+    <>
+    <h1>chai or react | Hamza Shoukat</h1>
+    <Chai />
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai";
+
+function App() {
+
+  return (
+  <> 
+  
+  <h1>Chai or react with vite | Hamza Shoukat</h1>
+  <Chai />
+
+  </>
+  );
+}
+
+export default App
